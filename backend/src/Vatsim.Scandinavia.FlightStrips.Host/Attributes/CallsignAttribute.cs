e5332761ee73ccf024667e742a4dfe7d2de2eaf0
Frederik Rosenberg <frederik@schultz-rosenberg.dk>
2023-10-21 19:05:03 +0200

    EfCore

---
+++ backend/src/Vatsim.Scandinavia.FlightStrips.Host/Attributes/CallsignAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vatsim.Scandinavia.FlightStrips.Host.Attributes;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
+public class CallsignAttribute : RegularExpressionAttribute
+{
+    private const string Regex = "^[a-ZA-Z0-9]*$";
+
+    public CallsignAttribute() : base(Regex)
+    {
+    }
+}
---
+++ backend/src/Vatsim.Scandinavia.FlightStrips.Persistence.EfCore/IAirportAndSessionTenant.cs
@@ -0,0 +1,8 @@
+﻿using Vatsim.Scandinavia.FlightStrips.Abstractions;
+
+namespace Vatsim.Scandinavia.FlightStrips.Persistence.EfCore;
+
+public interface IAirportAndSessionTenant : IAirportTenant, ISessionTenant
+{
+
+}

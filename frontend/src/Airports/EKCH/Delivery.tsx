84298af3310134f7433db250f928faca69dde646
Simon Bjerre <simon.bjerre@team.blue>
2024-01-13 20:35:48 +0100

    bay strip map

--- frontend/src/Airports/EKCH/Delivery.tsx
+++ frontend/src/Airports/EKCH/Delivery.tsx
@@ -34,15 +34,27 @@ const Delivery = observer(() => {
         </div>
         <div className="bg-[#555355] w-full h-auto border-l-4 border-r-4 border-[#a9a9a9]">
           <BayHeader title="CLEARED" />
-          <div className="h-[calc(50%-2.5rem)] overflow-auto overflow-x-hidden"></div>
+          <div className="h-[calc(50%-2.5rem)] overflow-auto overflow-x-hidden">
+            {flightStripStore.inBay('cleared').map((item) => (
+              <FlightStrip strip={item} />
+            ))}
+          </div>
           <BayHeader title="MESSAGES" msg />
           <div className="h-[calc(33%-2.5rem)] overflow-auto overflow-x-hidden"></div>
         </div>
         <div className="bg-[#555355] w-full h-auto border-l-2 border-[#a9a9a9]">
           <BayHeader title="PUSHBACK" />
-          <div className="h-[calc(33%-2.5rem)] overflow-auto overflow-x-hidden"></div>
+          <div className="h-[calc(33%-2.5rem)] overflow-auto overflow-x-hidden">
+            {flightStripStore.inBay('PUSHBACK').map((item) => (
+              <FlightStrip strip={item} />
+            ))}
+          </div>
           <BayHeader title="TWY DEP" />
-          <div className="h-[calc(66%-2.5rem)] overflow-auto overflow-x-hidden"></div>
+          <div className="h-[calc(66%-2.5rem)] overflow-auto overflow-x-hidden">
+            {flightStripStore.inBay('TWY DEP').map((item) => (
+              <FlightStrip strip={item} />
+            ))}
+          </div>
         </div>
       </div>
       <CommandBar />

9d32767f07754127d1066c333b8e3fdbe7734a4e
Frederik Rosenberg <frederik@schultz-rosenberg.dk>
2024-02-09 16:49:26 +0100

    Fix backend build

--- backend/src/Vatsim.Scandinavia.FlightStrips.Abstractions/Positions/Position.cs
+++ backend/src/Vatsim.Scandinavia.FlightStrips.Abstractions/Positions/Position.cs
@@ -8,7 +8,7 @@ public record struct Position(string Frequency)
     public static readonly Position EKCH_S_GND = new ("");
     public static readonly Position EKCH_A_GND = new ("121.630");
     public static readonly Position EKCH_D_GND = new ("121.730");
-    public static readonly Position EKCH_A_TWR = new ("118.108");
+    public static readonly Position EKCH_A_TWR = new ("118.105");
     public static readonly Position EKCH_D_TWR = new ("119.335");
     public static readonly Position EKCH_C_TWR = new ("118.580");
     public static readonly Position EKCH_DEL = new ("119.905");

5b010d501debb27cbdb5dd9f80a24d7c6abd0b81
Simon Bjerre <bjerrecs@protonmail.com>
2024-12-29 23:15:08 +0100

    added time component

---
+++ web/src/app/helpers/time.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
+
+const CurrentUTC: React.FC = () => {
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const formattedDate = `${date.getUTCHours().toString().padStart(2, '0')}:${date.getUTCMinutes().toString().padStart(2, '0')}:${date.getUTCSeconds().toString().padStart(2, '0')}Z`;
+    return <div>{formattedDate}</div>;
+};
+
+export { CurrentUTC };

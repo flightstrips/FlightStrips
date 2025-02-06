67ca0a5e20e2717236d418948de5e1c8e2515578
Threpio <Theomorris@hotmail.co.uk>
2025-02-06 10:48:13 +0100

    Test Updates

--- golang/euroscope_handlers.go
+++ golang/euroscope_handlers.go
@@ -1,28 +1,28 @@
 package main
 
 import (
+	"errors"
 	"github.com/MicahParks/keyfunc/v3"
 	"github.com/golang-jwt/jwt/v5"
 	"log"
 )
 
-func (s *Server) euroscopehandlerAuthentication(event EuroscopeAuthenticationEvent) (token *jwt.Token, err error) {
+func (s *Server) euroscopehandlerAuthentication(event EuroscopeAuthenticationEvent, authServerURL string) (token *jwt.Token, err error) {
 
 	// TODO: Sort out Logging
 	JWTToken := event.Token
 	// TODO: Parameterize the server URL
-	serverURL := "https://dev-xd0uf4sd1v27r8tg.eu.auth0.com/.well-known/jwks.json"
-	
-	k, err := keyfunc.NewDefault([]string{serverURL}) // Context is used to end the refresh goroutine.
+
+	k, err := keyfunc.NewDefault([]string{authServerURL})
 	if err != nil {
 		log.Fatalf("Failed to create a keyfunc.Keyfunc from the server's URL.\nError: %s", err)
 	}
 	token, err = jwt.Parse(JWTToken, k.Keyfunc)
 	if err != nil {
-		log.Fatalf("Failed to parse the JWT.\nError: %s", err)
+		return token, err
 	}
 	if !token.Valid {
-		log.Fatalf("The JWT is invalid.")
+		return token, errors.New("invalid jwt")
 	}
 
 	return token, nil

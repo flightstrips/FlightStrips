ecee7a81226c05c8db946759b445579cfcf57bee
Frederik Rosenberg <frederik@schultz-rosenberg.dk>
2026-02-25 21:41:19 +0100

    More hooks

--- frontend/src/store/airports/ekch.ts
+++ frontend/src/store/airports/ekch.ts
@@ -1,5 +1,5 @@
 import {useMemo} from "react";
-import {useWebSocketStore} from "@/store/store-hooks.ts";
+import {useAirport, useWebSocketStore} from "@/store/store-hooks.ts";
 import {Bay, type FrontendStrip} from "@/api/models.ts";
 
 const isSasStrip = (strip: FrontendStrip) =>
@@ -41,6 +41,90 @@ export const useOtherBayStrips = () => {
           !isSasStrip(x) &&
           !isNorwegianStrip(x)
       ),
+    [strips]
+  );
+};
+
+export const usePushbackStrips = () => {
+  const strips = useWebSocketStore(state => state.strips);
+  return useMemo(
+    () => strips.filter(x => x.bay === Bay.Push).sort((a, b) => a.sequence - b.sequence),
+    [strips]
+  );
+};
+
+export const useTaxiDepStrips = () => {
+  const strips = useWebSocketStore(state => state.strips);
+  const airport = useAirport();
+  return useMemo(
+    () =>
+      strips
+        .filter(x => x.bay === Bay.Taxi && x.origin === airport)
+        .sort((a, b) => a.sequence - b.sequence),
+    [strips, airport]
+  );
+};
+
+export const useTaxiArrStrips = () => {
+  const strips = useWebSocketStore(state => state.strips);
+  const airport = useAirport();
+  return useMemo(
+    () =>
+      strips
+        .filter(x => x.bay === Bay.Taxi && x.destination === airport)
+        .sort((a, b) => a.sequence - b.sequence),
+    [strips, airport]
+  );
+};
+
+export const useDepartStrips = () => {
+  const strips = useWebSocketStore(state => state.strips);
+  return useMemo(
+    () => strips.filter(x => x.bay === Bay.Depart).sort((a, b) => a.sequence - b.sequence),
+    [strips]
+  );
+};
+
+export const useAirborneStrips = () => {
+  const strips = useWebSocketStore(state => state.strips);
+  return useMemo(
+    () => strips.filter(x => x.bay === Bay.Airborne).sort((a, b) => a.sequence - b.sequence),
+    [strips]
+  );
+};
+
+export const useFinalStrips = () => {
+  const strips = useWebSocketStore(state => state.strips);
+  return useMemo(
+    () => strips.filter(x => x.bay === Bay.Final).sort((a, b) => a.sequence - b.sequence),
+    [strips]
+  );
+};
+
+export const useRwyArrStrips = () => {
+  const strips = useWebSocketStore(state => state.strips);
+  const airport = useAirport();
+  return useMemo(
+    () =>
+      strips
+        .filter(x => x.bay === Bay.Final && x.destination === airport)
+        .sort((a, b) => a.sequence - b.sequence),
+    [strips, airport]
+  );
+};
+
+export const useStandStrips = () => {
+  const strips = useWebSocketStore(state => state.strips);
+  return useMemo(
+    () => strips.filter(x => x.bay === Bay.Stand).sort((a, b) => a.sequence - b.sequence),
+    [strips]
+  );
+};
+
+export const useHiddenStrips = () => {
+  const strips = useWebSocketStore(state => state.strips);
+  return useMemo(
+    () => strips.filter(x => x.bay === Bay.Hidden).sort((a, b) => a.sequence - b.sequence),
     [strips]
   );
 };

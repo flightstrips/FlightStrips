97c320ba17d7a1d12ccbf2d27db5e988f8a19131
Frederik Rosenberg <frederik@schultz-rosenberg.dk>
2026-03-01 21:30:07 +0100

    fix: Use correct event for strip updates

--- backend/internal/frontend/handlers.go
+++ backend/internal/frontend/handlers.go
@@ -154,6 +154,7 @@ func handleStripUpdate(ctx context.Context, client *Client, message Message) err
 	}
 
 	if event.Eobt != nil && strip.Eobt != event.Eobt {
+		slog.Warn("EOBT updates are currently not supported and will be ignored", slog.String("callsign", event.Callsign))
 		// TODO add support
 	}
 
